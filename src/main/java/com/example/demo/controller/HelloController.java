829dcb81854af6d6f1e5fae24ce3f05b1bd92569
huguiqi <guiqi.hu@163.com>
2017-07-28 17:57:35 +0800

    first commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by guiqi on 2017/7/28.
+ */
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
